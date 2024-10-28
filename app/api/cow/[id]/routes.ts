514439173c4a555476488d8c29fa6a0d70e307b9
LINUX-NDOB <lordndob@gmail.com>
2024-10-27 23:40:46 -0500

    added NextRequestUrl

--- app/api/cow/[id]/routes.ts
+++ app/api/cow/[id]/routes.ts
@@ -1,14 +1,9 @@
-import { NextRequest, NextResponse } from "next/server";
-import { db } from "../../../db/index";
-import { cows } from "../../../db/schema";
-import { eq } from "drizzle-orm";
-
-interface Params {
-  id: string;
-}
-export function GET(request: Request, { params }: { params: Params }) {
-  const content = request.body;
-  const user_id = params.id;
-  console.log(content);
-}
-
+//import { NextRequest, NextResponse } from "next/server";
+//import { db } from "../../../db/index";
+//import { cows } from "../../../db/schema";
+//import { eq } from "drizzle-orm";
+//
+//export function GET(request: NextRequest) {
+//  const content = request.body;
+//  console.log(content);
+//}
--- app/api/user/[id]/route.ts
+++ app/api/user/[id]/route.ts
@@ -3,10 +3,6 @@ import { db } from "../../../db/index";
 import { profiles } from "../../../db/schema";
 import { eq } from "drizzle-orm";
 
-interface Params {
-    id: string;
-}
-
 interface ProfileResponse {
   user_id: string;
   user_nit: string | null;
@@ -16,34 +12,52 @@ interface ProfileResponse {
   phone_number: string | null;
 }
 
-export async function GET(request: NextRequest, { params } : { params: Params}) {
-    const user_id = params.id;
+export async function GET(request: NextRequest) {
+  const user_id = request.nextUrl.searchParams.get("id");
 
-    const user : ProfileResponse[] = await db.select().from(profiles).where(eq(profiles.user_id, user_id));
+  if (!user_id) {
+    return NextResponse.json(
+      { error: "User ID not provided" },
+      { status: 400 },
+    );
+  }
 
-    return NextResponse.json(user[0]);
+  const user: ProfileResponse[] = await db
+    .select()
+    .from(profiles)
+    .where(eq(profiles.user_id, user_id));
+
+  return NextResponse.json(user[0]);
 }
 
-interface ProfileRequest {
-  user_nit: string;
-  user_name: string;
-  user_lastname: string;
-  date_of_birth: string;
-  phone_number: string;
-}
-
-export async function PUT(request: ProfileRequest, { params } : { params: Params}){
-    const user_id = params.id;
-
-    const insert = await db.update(profiles).set(request).where(eq(profiles.user_id, user_id));
-
-    return NextResponse.json({ message: "updated"});
-}
-
-export async function DELETE(request: Request, { params }: { params: Params}){
-    const user_id = params.id;
-    
-    const delete_user = await db.delete(profiles).where(eq(profiles.user_id, user_id));
-
-    return NextResponse.json({message: "Deleted"});
-}
+//interface ProfileRequest {
+//  user_nit: string;
+//  user_name: string;
+//  user_lastname: string;
+//  date_of_birth: string;
+//  phone_number: string;
+//}
+//
+//export async function PUT(
+//  request: ProfileRequest,
+//  { params }: { params: Params },
+//) {
+//  const user_id = params.id;
+//
+//  const insert = await db
+//    .update(profiles)
+//    .set(request)
+//    .where(eq(profiles.user_id, user_id));
+//
+//  return NextResponse.json({ message: "updated" });
+//}
+//
+//export async function DELETE(request: Request, { params }: { params: Params }) {
+//  const user_id = params.id;
+//
+//  const delete_user = await db
+//    .delete(profiles)
+//    .where(eq(profiles.user_id, user_id));
+//
+//  return NextResponse.json({ message: "Deleted" });
+//}
